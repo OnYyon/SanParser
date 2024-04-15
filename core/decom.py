0541649b3e75d36ad0e6566b9bde3e6bc02abb1b
OnYyon <oleg.school@mai.ru>
2024-04-15 22:05:42 +0300

    add file for decompression files

---
+++ core/decom.py
@@ -0,0 +1,41 @@
+import os
+import gzip
+import fnmatch
+
+
+def switch_or_director(path_to_file: str):
+    try:
+        lst = ["*S0*SSHOW_SYS*.txt.gz", "*S0*SSHOW_SEC*.txt.gz", "*S0*SSHOW_FABRIC*.txt.gz",
+               "*S0*SSHOW_SERVICE*.txt.gz"]
+        for i in lst:
+            files = fnmatch.filter(os.listdir(f"./_in/{path_to_file}"), i)[0]
+            with gzip.GzipFile(f"./_in/{path_to_file}/{files}", "rb") as dec_file:
+                string = dec_file.read()
+                with open(f"_in/{path_to_file}/{files.replace('.gz', '')}", "wb") as f:
+                    f.write(string.decode())
+    except IndexError:
+        files = fnmatch.filter(os.listdir(f"./_in/{path_to_file}"), "*SSHOW_SYS*.txt.gz")[0]
+        with gzip.GzipFile(f"./_in/{path_to_file}/{files}", "r") as dec_file:
+            flag = False
+            while True:
+                line = dec_file.readline()
+                if b"firmwareshow -v" in line:
+                    flag = True
+                    continue
+                if b"real" in line and flag:
+                    break
+                if flag:
+                    if b"Slot Name" in line:
+                        continue
+                    else:
+                        if b"ACTIVE" in line:
+                            slot = line.split()[0]
+        slot = slot.decode()
+        lst = [f"*S{slot}*SSHOW_SYS*.txt.gz", f"*S{slot}*SSHOW_SEC*.txt.gz", f"*S{slot}*SSHOW_FABRIC*.txt.gz",
+               f"*S{slot}*SSHOW_SERVICE*.txt.gz"]
+        for i in lst:
+            files = fnmatch.filter(os.listdir(f"./_in/{path_to_file}"), i)[0]
+            with gzip.GzipFile(f"./_in/{path_to_file}/{files}", "rb") as dec_file:
+                string = dec_file.read()
+                with open(f"_in/{path_to_file}/{files.replace('.gz', '')}", "wb") as f:
+                    f.write(string)
